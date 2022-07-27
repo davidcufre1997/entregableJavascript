649e10560e00d8c03ce2f870cbb467f903361033
davidcufre1997 <107009875+davidcufre1997@users.noreply.github.com>
2022-07-27 14:29:18 -0300

    Se sufe con correcciones

---
+++ archivo.js
@@ -0,0 +1,11 @@
+
+
+
+
+function saqueSuTurno(){
+    var mensaje;
+var opcion = prompt("Introduzca su nombre:");
+for (let i=0; i<1; i++){
+    alert ("Buenos dias " + opcion+ ", le informamos que su turno es el Nº " + i+ " con el doctor Saravia. Dia Lunes a partir de las 8:00")}} {
+   
+}
